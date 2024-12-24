d7d2ec36e4faecb569d7353d69094289012cae22
Adrian PK <adrianpk@adrianpk.com>
2024-12-24 01:54:25 +0100

    Implement router

--- internal/am/router.go
+++ internal/am/router.go
@@ -1,1 +1,24 @@
 package am
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+type Router struct {
+	chi.Router
+	log Logger
+}
+
+func NewRouter(logger Logger) *Router {
+	return &Router{
+		Router: chi.NewRouter(),
+		log:    logger,
+	}
+}
+
+func (r *Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	r.log.Info(req.Method, " ", req.URL.Path)
+	r.Router.ServeHTTP(w, req)
+}
--- main.go
+++ main.go
@@ -3,14 +3,13 @@ package main
 import (
 	"context"
 	"errors"
-	"log"
 	"net/http"
 	"os"
 	"os/signal"
 	"time"
 
+	"github.com/aquamarinepk/todo/internal/am"
 	"github.com/aquamarinepk/todo/internal/feat/todo"
-	"github.com/go-chi/chi/v5"
 )
 
 func main() {
@@ -20,10 +19,12 @@ func main() {
 	webHandler := todo.NewHandler(service)
 	apiHandler := todo.NewAPIHandler(service)
 
-	webRouter := chi.NewRouter()
+	log := am.NewLogger("info")
+
+	webRouter := am.NewRouter(log)
 	webRouter.Mount("/todo", todo.NewWebRouter(webHandler))
 
-	apiRouter := chi.NewRouter()
+	apiRouter := am.NewRouter(log)
 	apiRouter.Mount("/api/todo", todo.NewAPIRouter(apiHandler))
 
 	webServer := &http.Server{
@@ -40,18 +41,18 @@ func main() {
 	signal.Notify(stop, os.Interrupt)
 
 	go func() {
-		log.Println("Starting web server on :8080")
+		log.Info("Starting web server on :8080")
 		err := webServer.ListenAndServe()
 		if err != nil && !errors.Is(err, http.ErrServerClosed) {
-			log.Fatalf("Could not listen on :8080: %v\n", err)
+			log.Errorf("Could not listen on :8080: %v\n", err)
 		}
 	}()
 
 	go func() {
-		log.Println("Starting API server on :8081")
+		log.Info("Starting API server on :8081")
 		err := apiServer.ListenAndServe()
 		if err != nil && !errors.Is(err, http.ErrServerClosed) {
-			log.Fatalf("Could not listen on :8081: %v\n", err)
+			log.Errorf("Could not listen on :8081: %v\n", err)
 		}
 	}()
 
@@ -60,15 +61,15 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	log.Println("Shutting down web server...")
+	log.Info("Shutting down web server...")
 	if err := webServer.Shutdown(ctx); err != nil {
-		log.Fatalf("Web server forced to shutdown: %v", err)
+		log.Errorf("Web server forced to shutdown: %v", err)
 	}
 
-	log.Println("Shutting down API server...")
+	log.Info("Shutting down API server...")
 	if err := apiServer.Shutdown(ctx); err != nil {
-		log.Fatalf("API server forced to shutdown: %v", err)
+		log.Errorf("API server forced to shutdown: %v", err)
 	}
 
-	log.Println("Servers stopped gracefully")
+	log.Info("Servers stopped gracefully")
 }
