75b9e270ec2f5d90a853d810ada21c7faf955a24
Adrian PK <adrianpk@adrianpk.com>
2025-01-29 19:02:54 +0100

    Implement colored log output

--- internal/am/log.go
+++ internal/am/log.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"strings"
+	"time"
 	"unicode"
 )
 
@@ -15,6 +16,21 @@ const (
 	DebugLevel LogLevel = iota
 	InfoLevel
 	ErrorLevel
+)
+
+const (
+	colorReset     = "\033[0m"
+	colorRed       = "\033[31m"
+	colorGreen     = "\033[32m"
+	colorYellow    = "\033[33m"
+	colorBlue      = "\033[34m"
+	colorTimestamp  = "\033[90m"
+)
+
+const (
+	debugPrefix = colorBlue + "[DBG] " + colorReset
+	infoPrefix  = colorGreen + "[INF] " + colorReset
+	errorPrefix = colorRed + "[ERR] " + colorReset
 )
 
 type Logger interface {
@@ -37,11 +53,15 @@ type BaseLogger struct {
 func NewLogger(logLevel string) *BaseLogger {
 	level := ToValidLevel(logLevel)
 	return &BaseLogger{
-		debug:    log.New(os.Stdout, "[DBG] ", log.LstdFlags),
-		info:     log.New(os.Stdout, "[INF] ", log.LstdFlags),
-		error:    log.New(os.Stderr, "[ERR] ", log.LstdFlags),
+		debug:    newCustomLogger(os.Stdout, debugPrefix, level, DebugLevel),
+		info:     newCustomLogger(os.Stdout, infoPrefix, level, InfoLevel),
+		error:    newCustomLogger(os.Stderr, errorPrefix, level, ErrorLevel),
 		logLevel: level,
 	}
+}
+
+func newCustomLogger(out *os.File, prefix string, currentLevel, level LogLevel) *log.Logger {
+	return log.New(out, "", 0)
 }
 
 func (l *BaseLogger) SetLogLevel(level LogLevel) {
@@ -50,42 +70,47 @@ func (l *BaseLogger) SetLogLevel(level LogLevel) {
 
 func (l *BaseLogger) Debug(v ...any) {
 	if l.logLevel <= DebugLevel {
-		l.debug.Println(v...)
+		l.debug.Output(2, formatLogMessage(debugPrefix, v...))
 	}
 }
 
 func (l *BaseLogger) Debugf(format string, a ...any) {
 	if l.logLevel <= DebugLevel {
 		message := fmt.Sprintf(format, a...)
-		l.debug.Println(message)
+		l.debug.Output(2, formatLogMessage(debugPrefix, message))
 	}
 }
 
 func (l *BaseLogger) Info(v ...any) {
 	if l.logLevel <= InfoLevel {
-		l.info.Println(v...)
+		l.info.Output(2, formatLogMessage(infoPrefix, v...))
 	}
 }
 
 func (l *BaseLogger) Infof(format string, a ...any) {
 	if l.logLevel <= InfoLevel {
 		message := fmt.Sprintf(format, a...)
-		l.info.Println(message)
+		l.info.Output(2, formatLogMessage(infoPrefix, message))
 	}
 }
 
-func (l *BaseLogger) Error(v ...interface{}) {
+func (l *BaseLogger) Error(v ...any) {
 	if l.logLevel <= ErrorLevel {
 		message := fmt.Sprint(v...)
-		l.error.Println(message)
+		l.error.Output(2, formatLogMessage(errorPrefix, message))
 	}
 }
 
-func (l *BaseLogger) Errorf(format string, a ...interface{}) {
+func (l *BaseLogger) Errorf(format string, a ...any) {
 	if l.logLevel <= ErrorLevel {
 		message := fmt.Sprintf(format, a...)
-		l.error.Println(message)
+		l.error.Output(2, formatLogMessage(errorPrefix, message))
 	}
+}
+
+func formatLogMessage(prefix string, v ...any) string {
+	timestamp := time.Now().Format("2006/01/02 15:04:05")
+	return fmt.Sprintf("%s%s%s %s%s", prefix, colorTimestamp, timestamp, colorReset, fmt.Sprint(v...))
 }
 
 func ToValidLevel(level string) LogLevel {
@@ -106,19 +131,19 @@ func ToValidLevel(level string) LogLevel {
 // SetDebugOutput set the internal log.
 // Used for package testing.
 func (l *BaseLogger) SetDebugOutput(debug *bytes.Buffer) {
-	l.debug = log.New(debug, "", 1)
+	l.debug = log.New(debug, "", 0)
 }
 
 // SetInfoOutput set the internal log.
 // Used for package testing.
 func (l *BaseLogger) SetInfoOutput(info *bytes.Buffer) {
-	l.info = log.New(info, "", 1)
+	l.info = log.New(info, "", 0)
 }
 
 // SetErrorOutput set the internal log.
 // Used for package testing.
 func (l *BaseLogger) SetErrorOutput(error *bytes.Buffer) {
-	l.error = log.New(error, "", 1)
+	l.error = log.New(error, "", 0)
 }
 
 func capitalize(str string) string {
