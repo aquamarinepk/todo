aa18d85ad0f4b35e3b04037f5bfe9faa204ce18a
Adrian PK <adrianpk@adrianpk.com>
2025-03-21 20:46:06 +0100

    Replace slug with UUID as primary identifier in requests and queries

--- internal/res/todo/service.go
+++ internal/res/todo/service.go
@@ -9,12 +9,10 @@ import (
 
 type Service interface {
 	GetLists(ctx context.Context) ([]List, error)
-	GetListByID(ctx context.Context, id uuid.UUID) (List, error)
-	GetListBySlug(ctx context.Context, slug string) (List, error)
-	CreateList(ctx context.Context, list List) error
-	UpdateList(ctx context.Context, list List) error
-	DeleteList(ctx context.Context, id uuid.UUID) error
-	DeleteListBySlug(ctx context.Context, slug string) error
+	Get(ctx context.Context, id uuid.UUID) (List, error)
+	Create(ctx context.Context, list List) error
+	Update(ctx context.Context, list List) error
+	Delete(ctx context.Context, id uuid.UUID) error
 }
 
 type BaseService struct {
@@ -33,34 +31,20 @@ func (svc *BaseService) GetLists(ctx context.Context) ([]List, error) {
 	return svc.repo.GetAll(ctx)
 }
 
-func (svc *BaseService) GetListByID(ctx context.Context, id uuid.UUID) (List, error) {
-	return svc.repo.GetByID(ctx, id)
+func (svc *BaseService) Get(ctx context.Context, id uuid.UUID) (List, error) {
+	return svc.repo.Get(ctx, id)
 }
 
-func (svc *BaseService) GetListBySlug(ctx context.Context, slug string) (List, error) {
-	return svc.repo.GetBySlug(ctx, slug)
-}
-
-func (svc *BaseService) CreateList(ctx context.Context, list List) error {
+func (svc *BaseService) Create(ctx context.Context, list List) error {
 	list.GenSlug()
 	list.GenCreationValues()
-	err := svc.repo.Create(ctx, list) // NOTE: Remove assignment to err and return the function call directly.
-	svc.repo.Debug()
-	return err
+	return svc.repo.Create(ctx, list)
 }
 
-func (svc *BaseService) UpdateList(ctx context.Context, list List) error {
+func (svc *BaseService) Update(ctx context.Context, list List) error {
 	return svc.repo.Update(ctx, list)
 }
 
-func (svc *BaseService) DeleteList(ctx context.Context, id uuid.UUID) error {
+func (svc *BaseService) Delete(ctx context.Context, id uuid.UUID) error {
 	return svc.repo.Delete(ctx, id)
-}
-
-func (svc *BaseService) DeleteListBySlug(ctx context.Context, slug string) error {
-	list, err := svc.repo.GetBySlug(ctx, slug)
-	if err != nil {
-		return err
-	}
-	return svc.repo.Delete(ctx, list.ID())
 }
