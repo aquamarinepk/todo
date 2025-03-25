30d8587fb51736cde9f0398643b01b53a0539956
Adrian PK <adrianpk@adrianpk.com>
2025-03-25 11:35:09 +0100

    Add auth related migrations

---
+++ assets/migration/sqlite/20250324082300-create-resources-table.sql
@@ -0,0 +1,16 @@
+-- +migrate Up
+CREATE TABLE resources (
+                           id TEXT PRIMARY KEY,
+                           name TEXT,
+                           description TEXT,
+                           label TEXT,
+                           type TEXT,
+                           uri TEXT,
+                           created_by TEXT,
+                           updated_by TEXT,
+                           created_at TIMESTAMP,
+                           updated_at TIMESTAMP
+);
+
+-- +migrate Down
+DROP TABLE resources;
