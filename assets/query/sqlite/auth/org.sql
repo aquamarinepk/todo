0df6530214bb90585e06c679131ee792fce51555
Adrian PK <adrianpk@adrianpk.com>
2025-06-12 22:35:05 +0200

    Implement owner handling

--- assets/query/sqlite/auth/org.sql
+++ assets/query/sqlite/auth/org.sql
@@ -7,3 +7,6 @@ VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);
 
 -- GetDefault
 SELECT id, short_id, name, short_description, description, created_by, updated_by, created_at, updated_at FROM org ORDER BY created_at ASC LIMIT 1;
+
+-- Delete
+DELETE FROM org WHERE id = ?;
